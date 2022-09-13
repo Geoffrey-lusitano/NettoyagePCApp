6809a436ae770cc778d3420ec2eda105695d44bb
geoffrey lusitano <g.bl@outlook.fr>
2022-09-13 14:49:04 +0200

    Ajout fonction analyser et nettoyer

--- NettoyagePCApp/MainWindow.xaml.cs
+++ NettoyagePCApp/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace NettoyagePCApp
 {
@@ -20,35 +22,132 @@ namespace NettoyagePCApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DirectoryInfo winTemp;
+        public DirectoryInfo appTemp;
         public MainWindow()
         {
             InitializeComponent();
+            winTemp = new DirectoryInfo(@"C:\Windows\Temp");
+            appTemp = new DirectoryInfo(System.IO.Path.GetTempPath());
             
         }
-        private void btn_Click(object sender, RoutedEventArgs e)
+
+        // Calcul de la taille d'un dossier
+        public long DirSize(DirectoryInfo dir)
         {
-            MessageBox.Show("Coucou");
-            fenetre.Close();
+            return dir.GetFiles().Sum(fi => fi.Length) + dir.GetDirectories().Sum(di => DirSize(di));
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // Vider un dossier
+        public void ClearTempData(DirectoryInfo di)
         {
+            foreach (FileInfo file in di.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                    Console.WriteLine(file.FullName);
+                    
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                try
+                {
+                    dir.Delete(true);
+                    Console.WriteLine(dir.FullName);
+
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+        }
+
+        private void Button_ANALYSER_Click(object sender, RoutedEventArgs e)
+        {
+            AnalyseFolders();
+        }
+
+        private void Button_CLEAN_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Nettoyage en cours...");
+            btnClean.Content = "NETTOYAGE EN COURS";
+            // Nettoyage du presse papier
+            Clipboard.Clear();
+            // Nettoyage des dossiers temp
+            try
+            {
+                ClearTempData(winTemp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
+
+            try
+            {
+                ClearTempData(appTemp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
+
+            btnClean.Content = "NETTOYAGE TERMINER";
+            title.Content = "Nettoyage effectuée";
+            space.Content = "0 Mb";
+        }
+
+        private void Button_MAJ_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Votre logiciel est à jour", "Mise à jour", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_HISTORIQUE_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO : Créer page historique", "Historique", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_WEB_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("http://geoffrey-lusitano.fr")
+                {
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        public void AnalyseFolders()
         {
+            Console.WriteLine("Debut de l'analyse...");
+            long totalSize = 0;
+            try
+            {
+                totalSize += DirSize(winTemp) / 1000000;
+                totalSize += DirSize(appTemp) / 1000000;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Analyse impossible : " + ex.Message);
+            }
 
-        }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-
+            space.Content = totalSize + " Mb";
+            title.Content = "Analyse effectuée";
+            date.Content = DateTime.Today;
         }
     }
 }
